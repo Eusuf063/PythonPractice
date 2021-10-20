5531acc4f79b22c08dcd2330339a6833c8ad0ba5
Eusuf <eusuf.hassan@gmail.com>
2021-10-20 22:42:43 +0300

    While loop practice_20.10.2021

---
+++ While Loop.py
@@ -0,0 +1,20 @@
+i = 1
+#while i<100:
+#    print(i)
+#    if i==60:
+#     break          #can be used for filter
+#    i=i+5
+
+#while i < 6:
+#    i += 1
+#    if i <= 3:
+#        continue     #can be used for filter
+#    print(i)
+
+print(" while with if else condition")
+
+while i <= 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
