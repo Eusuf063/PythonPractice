e04419af8d98616a9b1c0688f4a058f103c87f65
Eusuf <eusuf.hassan@gmail.com>
2021-10-20 23:10:24 +0300

    For loop practice_20.10.2021

---
+++ For Loop Practice.py
@@ -0,0 +1,7 @@
+car = ["Tesla","Toyota","Audi","Mazda","BMW","Honda"]
+fuel = ["Petrol","Octane","Gas","Disel"]
+
+print(' ============= Nested Loop=========')
+for a in car:
+    for b in fuel:
+        print(a,b)
