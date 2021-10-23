1eade96d03762443f6cd89ef4e29a86efbf735e5
Eusuf <eusuf.hassan@gmail.com>
2021-10-23 12:10:45 +0300

    Salary

---
+++ URI_Practice/1008.py
@@ -0,0 +1,6 @@
+NUMBER = int(input())
+WorkingHour = float(input())
+HourlyRate = float(input())
+SALARY = WorkingHour * HourlyRate
+print('NUMBER = '+str(NUMBER))
+print("SALARY = U$ %.2f" % SALARY)
